b299dddf5492cdc3a95a4746095fc2327099eada
chen8chen6 <chen8chen6@outlook.com>
2026-05-31 12:06:47 +0800

    修改CacheByNum策略, 改为已缓存数目低于阈值时, 一次载入多张图片

--- imgMgr/cacheByNum.cpp
+++ imgMgr/cacheByNum.cpp
@@ -33,18 +33,23 @@ int CCacheByNum::init(const QString &imgPath, std::shared_ptr<CFileMgr> fileMgr)
         m_cur = m_cache.begin();
 
         //向后缓存
-        QFileInfoList file2Cache = m_fileMgr->nFilesAfter(FORWARD_CACHE_SIZE, curImg);
+        //预计行为是向后浏览, 因此往这个反向多缓冲一些图片
+        //TODO: 调整缓冲顺序, 避免向前浏览时需要等待大量图片向后缓冲结束
+        QFileInfoList file2Cache = m_fileMgr->nFilesAfter(NEXT_CACHE_MOST, curImg);
         for (auto file : file2Cache)
             cacheNext(file);
+        m_nextCached = NEXT_CACHE_MOST;
 
         //向前缓存
-        file2Cache = m_fileMgr->nFilesBefore(BACKWARD_CACHE_SIZE, curImg);
+        file2Cache = m_fileMgr->nFilesBefore(PREV_CACHE_AVG, curImg);
         for (auto file : file2Cache)
             cachePrev(file);
+        m_prevCached = PREV_CACHE_AVG;
 
         qDebug() << "=== cache ===" ;
         for (const auto &imgFile : m_cache)
             qDebug() << imgFile->m_info.fileName();
+        qDebug() << "cached prev:" << m_prevCached << ", next:" << m_nextCached;
         qDebug() << "=============";
 
         isSucc = true;
@@ -62,26 +67,61 @@ int CCacheByNum::init(const QString &imgPath, std::shared_ptr<CFileMgr> fileMgr)
 pImgFile_t CCacheByNum::prev()
 {
     //维护缓冲区
-    qDebug() << "<-(cache): " << m_cache.back()->m_info.fileName();
-    m_cache.pop_back(); //向后缓冲区-1
-    auto file2Cache = m_fileMgr->fileBefore(m_cache.front()->m_info);
-    qDebug() << "(cache)<-: " << file2Cache.fileName();
-    cachePrev(file2Cache);  //向前缓冲区+1
-    --m_cur;
+    //向后缓存至多next_most个元素, 高于该水平则删减到next_lv个元素.
+    if (++m_nextCached > NEXT_CACHE_MOST)
+    {
+        for (int i = 0; i < m_nextCached - NEXT_CACHE_AVG; ++i)
+        {
+            qDebug() << "<-(cache): " << m_cache.back()->m_info.fileName();
+            m_cache.pop_back();
+        }
+        m_nextCached = NEXT_CACHE_AVG;
+    }
 
+    //向前缓存至少prev_least个元素, 低于该水平则补充到prev_lv个元素.
+    if (--m_prevCached < PREV_CACHE_LEAST)
+    {
+        auto fileList = m_fileMgr->nFilesBefore(PREV_CACHE_AVG - m_prevCached, m_cache.front()->m_info);
+        for (auto &file2Cache : fileList)
+        {
+            qDebug() << "(cache)<-: " << file2Cache.fileName();
+            cachePrev(file2Cache);
+        }
+        m_prevCached = PREV_CACHE_AVG;
+    }
+    qDebug() << "cached prev:" << m_prevCached << ", next:" << m_nextCached;
+    --m_cur;
     return *m_cur;
 }
 
 pImgFile_t CCacheByNum::next()
 {
     //维护缓冲区
-    qDebug() << "<-(cache): " << &m_cache.front() << m_cache.front()->m_info.fileName();
-    m_cache.pop_front(); //向前缓冲区-1
-    auto file2Cache = m_fileMgr->fileAfter(m_cache.back()->m_info);
-    qDebug() << "(cache)<-: " << file2Cache.fileName();
-    cacheNext(file2Cache);  //向后缓冲区+1
-    ++m_cur;
+    //向前缓存至多prev_most个元素, 高于该水平则删减到prev_lv个元素.
+    if (++m_prevCached > PREV_CACHE_MOST)
+    {
+        for (int i = 0; i < m_prevCached - PREV_CACHE_AVG; ++i)
+        {
+            qDebug() << "<-(cache): " << m_cache.front()->m_info.fileName();
+            m_cache.pop_front();
+        }
+        m_prevCached = PREV_CACHE_AVG;
+    }
 
+    //向后缓存至少next_least个元素, 低于该水平则补充到next_lv个元素.
+    if (--m_nextCached < NEXT_CACHE_LEAST)
+    {
+        auto fileList = m_fileMgr->nFilesAfter(NEXT_CACHE_AVG - m_nextCached, m_cache.back()->m_info);
+        for (auto& file2Cache : fileList)
+        {
+            qDebug() << "(cache)<-: " << file2Cache.fileName();
+            cacheNext(file2Cache);
+        }
+        m_nextCached = NEXT_CACHE_AVG;
+    }
+    qDebug() << "cached prev:" << m_prevCached << ", next:" << m_nextCached;
+
+    ++m_cur;
     return *m_cur;
 }
 
--- imgMgr/cacheByNum.h
+++ imgMgr/cacheByNum.h
@@ -3,7 +3,19 @@
 
 #include "imgMgr.h"
 
-//初始化完成后, 缓冲区始终都是满的, 且m_cur不会位于列表头尾
+/**
+* 缓冲区结构如下图所示:
+* 前<-----     ----->后
+* |prev_most|----|prev_avg|----|prev_least|----|cur|----|next_least|----|next_avg|----|next_most|
+* 
+* prev_most: 向前缓存至多prev_most个元素, 高于该水平则删减到prev_avg个元素.
+* prev_avg: 向前缓存的元素数量高于prev_most或低于prev_least时, 调整至prev_avg水平.
+* prev_least: 向前缓存至少prev_least个元素, 低于该水平则补充到prev_avg个元素.
+* cur: 当前指针
+* next_most: 向后缓存至少next_least个元素, 低于该水平则补充到next_avg个元素.
+* next_avg: 向后缓存的元素数量高于next_most或低于next_least时, 调整至next_avg水平.
+* next_most: 向后缓存至多next_most个元素, 高于该水平则删减到next_avg个元素.
+**/
 class CCacheByNum : public CImgMgr
 {
 public:
@@ -11,7 +23,8 @@ public:
     ~CCacheByNum() override;
 
     //缓冲池大小
-    static constexpr int cacheSize(void) {return FORWARD_CACHE_SIZE + BACKWARD_CACHE_SIZE + 1;}
+    //小于该数目的话, 会使用cacheAll策略, 因此这里不会出现向前向后缓冲到同一个文件的情况
+    static constexpr int cacheSize(void) {return NEXT_CACHE_MOST + PREV_CACHE_MOST + 1;}
 
     QString name(void) const override {return "imgMgr: Cache by num";}
     int init(const QString &imgPath, std::shared_ptr<CFileMgr> fileMgr) override;
@@ -20,8 +33,16 @@ public:
     pImgFile_t next(void) override;
 
 private:
-    static constexpr int FORWARD_CACHE_SIZE = 10;   //向后缓存数量
-    static constexpr int BACKWARD_CACHE_SIZE = 9;   //向前缓存数量
+    //向后缓存数量
+    static constexpr int NEXT_CACHE_LEAST = 10;
+    static constexpr int NEXT_CACHE_MOST = 20;
+    static constexpr int NEXT_CACHE_AVG = (NEXT_CACHE_MOST + NEXT_CACHE_LEAST) / 2;
+
+    //向前缓存数量
+    static constexpr int PREV_CACHE_LEAST = 5;
+    static constexpr int PREV_CACHE_MOST = 15;
+    static constexpr int PREV_CACHE_AVG = (PREV_CACHE_MOST + PREV_CACHE_LEAST) / 2;
+
     typedef std::list<pImgFile_t> cache_t;
 
 private:
@@ -30,9 +51,11 @@ private:
 
 private:
     cache_t m_cache;        //缓存
+    int m_nextCached = 0;   //向后已缓存数量
+    int m_prevCached = 0;   //向前已缓存数量
+
     cache_t::iterator m_cur;//当前图片文件
     std::shared_ptr<CFileMgr> m_fileMgr = nullptr;
-
 };
 
 #endif // CCACHEBYNUM_H
